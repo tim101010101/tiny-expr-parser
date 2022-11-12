4652a1f8f045b9e113eaa4219bb47f5042cc7147
Ethan Teng <1345118870@qq.com>
2022-11-12 19:28:44 +0800

    [feat] entry of the project

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,28 @@
+mod lexer;
+mod parser;
+mod syntax_kind;
+mod traversal;
+
+use lexer::lex;
+use parser::{syntax, Node};
+
+pub use traversal::{eval, format};
+
+/// build a AST from a expression
+pub fn build_ast(expr: &str) -> Result<Node, String> {
+    let root = syntax(lex(expr)?)?;
+    Ok(root)
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn smoke() {
+        let expr = "1 * 2 + (3 / (4  + (-5)))";
+        let ast = build_ast(expr).unwrap();
+        assert_eq!(-1, eval(&ast));
+        assert_eq!("1 * 2 + 3 / (4 + (-5))", format(&ast));
     }
 }
